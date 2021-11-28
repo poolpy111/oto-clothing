c4bd6c18792265a6e6b4eccf6e86306804aada33
unknown <poolpy111@gmail.com>
2021-11-28 14:56:07 +0400

    Added router to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>hats page</h1>
+      <p>Hi</p>
+    </div>
+  ) 
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="hats" element={<HatsPage />} />
+        </Routes>
+      </BrowserRouter>,
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShopPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ShopPage
